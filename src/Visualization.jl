e1cc432a663ea2a4d53d6a26e0579f31479e0f64
marteresagh <marteresa28@gmail.com>
2020-10-16 16:10:29 +0200

    fix

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -6,18 +6,21 @@ module Visualization
 	using Combinatorics
 
     # include code
-    dirs = readdir("src")
-	for dir in dirs
-		name = joinpath("src",dir)
-    	if isdir(name)
-			for (root,folders,files) in walkdir(name)
-				for file in files
-					head = splitdir(root)[2]
-					#@show joinpath(head,file)
-				 	include(joinpath(head,file))
-				end
-			end
-		end
-	end
+    # dirs = readdir("src")
+	# for dir in dirs
+	# 	name = joinpath("src",dir)
+    # 	if isdir(name)
+	# 		for (root,folders,files) in walkdir(name)
+	# 			for file in files
+	# 				head = splitdir(root)[2]
+	# 				#@show joinpath(head,file)
+	# 			 	include(joinpath(head,file))
+	# 			end
+	# 		end
+	# 	end
+	# end
+
+	include("View/color.jl")
+	include("View/geometrytools.jl")
  	export GL
 end # module
