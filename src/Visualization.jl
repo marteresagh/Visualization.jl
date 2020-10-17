0d952e677f3cf6913d9921878b276e1155849d86
marteresagh <marteresa28@gmail.com>
2020-10-17 16:15:21 +0200

    update package

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -6,21 +6,9 @@ module Visualization
 	using Combinatorics
 
     # include code
-    # dirs = readdir("src")
-	# for dir in dirs
-	# 	name = joinpath("src",dir)
-    # 	if isdir(name)
-	# 		for (root,folders,files) in walkdir(name)
-	# 			for file in files
-	# 				head = splitdir(root)[2]
-	# 				#@show joinpath(head,file)
-	# 			 	include(joinpath(head,file))
-	# 			end
-	# 		end
-	# 	end
-	# end
-
 	include("View/color.jl")
 	include("View/geometrytools.jl")
+
+	
  	export GL
 end # module
