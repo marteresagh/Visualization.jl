f48518c2fe5f60ed475d01af821777155ce11a8b
marteresagh <marteresa28@gmail.com>
2021-01-29 11:42:24 +0100

    normals

--- src/View/geometrytools.jl
+++ src/View/geometrytools.jl
@@ -1,14 +1,14 @@
 """
 Generate model of normals.
 """
-function mesh_normals(points::Lar.Points, normals::Lar.Points; s=1.::Float64)
+function normals(points::Lar.Points, normals::Lar.Points; s=1.::Float64)
 	@assert size(points,2) == size(normals,2) "mesh_normals: not valid input"
 	n = size(points,2)
 	norm = [[i,i+n] for i in 1:n]
 	totalpoints = hcat(points,normals)
 	tnormals = similar(normals)
 	for i in 1:n
-		tnormals[:,i] = points[:,i] + normals[:,i]
+		tnormals[:,i] = points[:,i] + s*normals[:,i]
 	end
 	totalpoints = hcat(points,tnormals)
 	return GL.GLGrid(totalpoints,norm,GL.Point4d(1,1,1,1))
